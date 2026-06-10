6baf50c3271467fb31305f9811fa5dbe28b9a5d1
champ <2756154878@qq.com>
2026-06-11 00:10:36 +0900

    fix(subscribe): change entityIds to tmplIds for WeChat parameter

--- src/shared/utils/subscribe.ts
+++ src/shared/utils/subscribe.ts
@@ -1,19 +1,23 @@
 import Taro from '@tarojs/taro'
 
+interface NotificationData {
+  [key: string]: string
+}
+
 export const SubscribeService = {
-  requestPermission: async (templateIds: string[]) => {
+  requestPermission: async (templateIds: string[]): Promise<Record<string, string> | null> => {
     try {
       const res = await Taro.requestSubscribeMessage({
-        entityIds: templateIds
+        tmplIds: templateIds
       })
-      return res
+      return res as Record<string, string>
     } catch (err) {
       console.error('订阅消息权限请求失败:', err)
       return null
     }
   },
 
-  sendNotification: async (userId: string, templateId: string, data: any) => {
+  sendNotification: async (userId: string, templateId: string, data: NotificationData) => {
     console.log('发送订阅消息:', { userId, templateId, data })
   }
 }
