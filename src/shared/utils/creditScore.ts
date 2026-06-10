2f7da0f59a3f40544001d168516da776d73a3638
champ <2756154878@qq.com>
2026-06-11 00:21:12 +0900

    feat(platform): add CreditScoreService utility

---
+++ src/shared/utils/creditScore.ts
@@ -0,0 +1,15 @@
+import Taro from '@tarojs/taro'
+
+export const CreditScoreService = {
+  requestAuth: async () => {
+    try {
+      const res = await Taro.getAuthCode({
+        scopes: 'auth_base'
+      })
+      return res
+    } catch (err) {
+      console.error('信用授权失败:', err)
+      return null
+    }
+  }
+}
